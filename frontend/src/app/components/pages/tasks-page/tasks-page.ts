505a71a424155a4b776cb42074ba61b9f98589b1
Darsh Shah <contactme@darshshah.com>
2026-03-01 20:41:03 +0000

    fix docker compose and frontend warning

--- frontend/src/app/components/pages/tasks-page/tasks-page.ts
+++ frontend/src/app/components/pages/tasks-page/tasks-page.ts
@@ -2,13 +2,12 @@ import { Component, signal } from '@angular/core';
 import { Header } from '../../header/header';
 import { TaskTable } from '../../task-table/task-table';
 import { Footer } from '../../footer/footer';
-import { Notifications } from '../../notifications/notifications';
 import { TaskCreateForm } from '../../task-create-form/task-create-form';
 import { LoginStatus } from '../../login-status/login-status';
 
 @Component({
   selector: 'app-tasks-page',
-  imports: [Header, TaskTable, Footer, Notifications, TaskCreateForm, LoginStatus],
+  imports: [Header, TaskTable, Footer, TaskCreateForm, LoginStatus],
   templateUrl: './tasks-page.html',
   styleUrl: './tasks-page.css',
 })
